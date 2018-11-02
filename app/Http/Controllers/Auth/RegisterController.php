a5613a27e82894d9547a7d4aa9255a5ab06e7d1d
Jonathan <jonathanperalta1893@gmail.com>
2018-11-01 21:38:41 -0500

    agragar campos al registro fill.user.guarded

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
+            'username' => 'required|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -65,8 +66,10 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'avatar' => 'q=tbn:ANd9GcR2hKudmXMJ9_SZTE3pYWfrZrAKoZLQ0MYZ1_lhYrVA8MGXY56g'
         ]);
     }
 }
